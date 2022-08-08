6927c5490dec2f24a95cbd3e53306477d277c995
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2022-08-07 19:06:59 -0600

    drop tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,4 @@
+drop table if exists Account;
+drop table if exists Password_Secret;
 CREATE TABLE if not exists Account (id UUID PRIMARY KEY, authentication_id varchar, email varchar, active boolean, access_date_time timestamp);
 create table if not exists Password_Secret(authentication_id varchar primary key, user_id uuid, secret varchar, expire_date timestamp);
