4f48e8c44a6bfbfb6bd065e86864d7a62c81e177
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-10-02 11:49:14 -0600

    update

--- src/main/java/me/sonam/account/Application.java
+++ src/main/java/me/sonam/account/Application.java
@@ -5,11 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
+@EnableDiscoveryClient
 @SpringBootApplication( scanBasePackages = {"me.sonam.account", "me.sonam.security"})
 public class Application {
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
